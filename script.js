ab11ae40dcfce95f50b42707cef117e56d357376
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-09-12 16:46:51 +0100

    base structure of game

--- script.js
+++ script.js
@@ -1,22 +1,99 @@
-const gameBoard = (() => {
-  let boardArray = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
-  let grid = document.getElementById("grid");
+let boardArray = ["", "", "", "", "", "", "", "", ""];
+const grid = document.querySelector("#grid");
+
+const game = (() => {
+  let currentSymbol = "o";
+  let round = 0;
   const renderBoard = () => {
     for (i = 0; i < boardArray.length; i++) {
-      let div = document.createElement("div");
-      div.classList.add("mark");
+      const div = document.createElement("div");
+      div.classList.add("symbol");
+      div.dataset.index = i;
       div.innerText = boardArray[i];
       grid.appendChild(div);
     }
   };
-  function putMark(symbol, position) {
-    boardArray[position] = symbol;
-  }
+
+  const addListeners = () => {
+    const markBtn = document.querySelectorAll(".symbol");
+
+    markBtn.forEach((e) => e.addEventListener("click", playRound));
+  };
+
+  const playRound = (e) => {
+    let index = e.target.getAttribute("data-index");
+
+    if (validate(index)) {
+      boardArray[index] = currentSymbol;
+      changeSymbol();
+      clearBoard();
+      renderBoard();
+      addListeners();
+      round++;
+      gameOver();
+    } else {
+      console.log("wrong position");
+    }
+  };
+
+  const changeSymbol = () => {
+    if (currentSymbol == "o") {
+      currentSymbol = "x";
+    } else {
+      currentSymbol = "o";
+    }
+  };
+  const clearBoard = () => {
+    grid.innerHTML = "";
+  };
+  const validate = (index) => (boardArray[index] == "" ? true : false);
+
+  const gameOver = () => {
+    if (winningCondition()) {
+      changeSymbol();
+      console.log(currentSymbol + " is a winner");
+    }
+    if (round == 9) {
+      console.log("Tie");
+    }
+  };
+
+  const winningCondition = () => {
+    if (
+      (boardArray[0] == boardArray[1] &&
+        boardArray[0] == boardArray[2] &&
+        boardArray[0] != "") ||
+      (boardArray[3] == boardArray[4] &&
+        boardArray[3] == boardArray[5] &&
+        boardArray[3] != "") ||
+      (boardArray[6] == boardArray[7] &&
+        boardArray[6] == boardArray[8] &&
+        boardArray[6] != "") ||
+      (boardArray[0] == boardArray[3] &&
+        boardArray[0] == boardArray[6] &&
+        boardArray[0] != "") ||
+      (boardArray[1] == boardArray[4] &&
+        boardArray[1] == boardArray[7] &&
+        boardArray[1] != "") ||
+      (boardArray[2] == boardArray[5] &&
+        boardArray[2] == boardArray[8] &&
+        boardArray[2] != "") ||
+      (boardArray[0] == boardArray[4] &&
+        boardArray[0] == boardArray[8] &&
+        boardArray[0] != "") ||
+      (boardArray[2] == boardArray[4] &&
+        boardArray[2] == boardArray[6] &&
+        boardArray[2] != "")
+    ) {
+      return true;
+    }
+  };
 
   return {
     renderBoard,
-    putMark,
+    addListeners,
   };
 })();
-gameBoard.putMark("o", 0);
-gameBoard.renderBoard();
+
+game.renderBoard();
+game.addListeners();
